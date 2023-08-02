7f19d790e908a85167bd1ae036323be2997e0f81
derrickdrye <dryd95@hotmail.com>
2023-08-02 15:13:25 -0500

    TacoParserExerciseComplete

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace LoggingKata.Test
 {
@@ -17,24 +18,29 @@ namespace LoggingKata.Test
             var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
 
             //Assert
-            Assert.NotNull(actual);
+            Assert.Equal(expected, actual.Location.Longitude);
 
         }
 
         [Theory]
-        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        public void ShouldParseLongitude(string line, double expected)
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLat(string line, double expected)
         {
             // TODO: Complete - "line" represents input data we will Parse to
             //       extract the Longitude.  Your .csv file will have many of these lines,
             //       each representing a TacoBell location
 
             //Arrange
+            var tacoParserInstance = new TacoParser();
 
             //Act
+            var actual = tacoParserInstance.Parse(line);
 
             //Assert
-        }
+            Assert.Equal(expected, actual.Location.Latitude);
+
+
+        }   
 
 
         //TODO: Create a test ShouldParseLatitude
